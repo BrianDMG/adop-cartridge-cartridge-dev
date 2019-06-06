9b2b04e432b0778954a820dee5f308a27ddbe570
Teague, Brian <brian.teague@accenture.com>
2019-06-06 15:28:55 -0400

    Changed syntax on scriptText()

--- jenkins/jobs/dsl/LoadTestCartridge.groovy
+++ jenkins/jobs/dsl/LoadTestCartridge.groovy
@@ -18,39 +18,36 @@ loadCartridgeJob.with{
           name('SCM_PROVIDER')
           choiceListProvider {
             systemGroovyChoiceListProvider {
-              scriptText('
-import hudson.model.*;
-import hudson.util.*;
+              scriptText('''import hudson.model.*;import hudson.util.*;
+                base_path = "/var/jenkins_home/userContent/datastore/pluggable/scm"
 
-base_path = "/var/jenkins_home/userContent/datastore/pluggable/scm"
+                // Initialise folder containing all SCM provider properties files
+                String PropertiesPath = base_path + "/ScmProviders/"
+                File folder = new File(PropertiesPath)
+                def providerList = []
 
-// Initialise folder containing all SCM provider properties files
-String PropertiesPath = base_path + "/ScmProviders/"
-File folder = new File(PropertiesPath)
-def providerList = []
+                // Loop through all files in properties data store and add to returned list
+                for (File fileEntry : folder.listFiles()) {
+                  if (!fileEntry.isDirectory()){
+                    String title = PropertiesPath +  fileEntry.getName()
+                    Properties scmProperties = new Properties()
+                    InputStream input = null
+                    input = new FileInputStream(title)
+                    scmProperties.load(input)
+                    String url = scmProperties.getProperty("scm.url")
+                    String protocol = scmProperties.getProperty("scm.protocol")
+                    String id = scmProperties.getProperty("scm.id")
+                    String output = url + " - " + protocol + " (" + id + ")"
+                    providerList.add(output)
+                  }
+                }
 
-// Loop through all files in properties data store and add to returned list
-for (File fileEntry : folder.listFiles()) {
-  if (!fileEntry.isDirectory()){
-    String title = PropertiesPath +  fileEntry.getName()
-    Properties scmProperties = new Properties()
-    InputStream input = null
-    input = new FileInputStream(title)
-    scmProperties.load(input)
-    String url = scmProperties.getProperty("scm.url")
-    String protocol = scmProperties.getProperty("scm.protocol")
-    String id = scmProperties.getProperty("scm.id")
-    String output = url + " - " + protocol + " (" + id + ")"
-    providerList.add(output)
-  }
-}
+                if (providerList.isEmpty()) {
+                    providerList.add("No SCM providers found")
+                }
 
-if (providerList.isEmpty()) {
-    providerList.add("No SCM providers found")
-}
-
-return providerList;
-')
+                return providerList;
+                ''')
               defaultChoice('Top')
               usePredefinedVariables(false)
             }
