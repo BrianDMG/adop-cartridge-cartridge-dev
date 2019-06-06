03bbbe55b0b407c42f340e291e83c565cd5ba17b
Teague, Brian <brian.teague@accenture.com>
2019-06-06 15:31:52 -0400

    Changed syntax on scriptText()

--- jenkins/jobs/dsl/LoadTestCartridge.groovy
+++ jenkins/jobs/dsl/LoadTestCartridge.groovy
@@ -18,36 +18,39 @@ loadCartridgeJob.with{
           name('SCM_PROVIDER')
           choiceListProvider {
             systemGroovyChoiceListProvider {
-              scriptText('''import hudson.model.*;import hudson.util.*;
-                base_path = "/var/jenkins_home/userContent/datastore/pluggable/scm"
+              groovyScript('''
+import hudson.model.*;
+import hudson.util.*;
 
-                // Initialise folder containing all SCM provider properties files
-                String PropertiesPath = base_path + "/ScmProviders/"
-                File folder = new File(PropertiesPath)
-                def providerList = []
+base_path = "/var/jenkins_home/userContent/datastore/pluggable/scm"
 
-                // Loop through all files in properties data store and add to returned list
-                for (File fileEntry : folder.listFiles()) {
-                  if (!fileEntry.isDirectory()){
-                    String title = PropertiesPath +  fileEntry.getName()
-                    Properties scmProperties = new Properties()
-                    InputStream input = null
-                    input = new FileInputStream(title)
-                    scmProperties.load(input)
-                    String url = scmProperties.getProperty("scm.url")
-                    String protocol = scmProperties.getProperty("scm.protocol")
-                    String id = scmProperties.getProperty("scm.id")
-                    String output = url + " - " + protocol + " (" + id + ")"
-                    providerList.add(output)
-                  }
-                }
+// Initialise folder containing all SCM provider properties files
+String PropertiesPath = base_path + "/ScmProviders/"
+File folder = new File(PropertiesPath)
+def providerList = []
 
-                if (providerList.isEmpty()) {
-                    providerList.add("No SCM providers found")
-                }
+// Loop through all files in properties data store and add to returned list
+for (File fileEntry : folder.listFiles()) {
+  if (!fileEntry.isDirectory()){
+    String title = PropertiesPath +  fileEntry.getName()
+    Properties scmProperties = new Properties()
+    InputStream input = null
+    input = new FileInputStream(title)
+    scmProperties.load(input)
+    String url = scmProperties.getProperty("scm.url")
+    String protocol = scmProperties.getProperty("scm.protocol")
+    String id = scmProperties.getProperty("scm.id")
+    String output = url + " - " + protocol + " (" + id + ")"
+    providerList.add(output)
+  }
+}
 
-                return providerList;
-                ''')
+if (providerList.isEmpty()) {
+    providerList.add("No SCM providers found")
+}
+
+return providerList;
+''')
               defaultChoice('Top')
               usePredefinedVariables(false)
             }
